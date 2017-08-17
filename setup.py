30a7adb19a974e9fe896f1d61a3cc4c2eb6e309a
yulai.li <yulai.li@speedycloud.cn>
2017-08-17 14:08:39 +0800

    Update version

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ long_description = """TiDB backend for Django"""
 
 setup(
     name='django_tidb',
-    version='1.0',
+    version='2.0',
     author='Rain Li',
     author_email='blacktear23@gmail.com',
     url='http://github.com/blacktear23/django_tidb',
-    download_url='http://github.com/blackear23/django_tidb/archive/1.0.tar.gz',
+    download_url='http://github.com/blackear23/django_tidb/archive/2.0.tar.gz',
     description='TiDB backend for Django',
     long_description=long_description,
     keywords=['django', 'tidb'],

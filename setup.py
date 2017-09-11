b615083c159d0c542ef1632810f3ca5c892d7175
yulai.li <yulai.li@speedycloud.cn>
2017-09-11 12:31:02 +0800

    Update README and update setup.py

--- setup.py
+++ setup.py
@@ -6,21 +6,22 @@ long_description = """TiDB backend for Django"""
 
 setup(
     name='django_tidb',
-    version='2.0',
+    version='2.1',
     author='Rain Li',
     author_email='blacktear23@gmail.com',
     url='http://github.com/blacktear23/django_tidb',
-    download_url='http://github.com/blackear23/django_tidb/archive/2.0.tar.gz',
+    download_url='http://github.com/blackear23/django_tidb/archive/2.1.tar.gz',
     description='TiDB backend for Django',
     long_description=long_description,
     keywords=['django', 'tidb'],
     packages=['django_tidb', 'django_tidb.tidb'],
     license='Apache2',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Software Development :: Libraries',
     ],
 )

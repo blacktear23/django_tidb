e27f68451ebd58736186ec0bc0b201b60988174d
yulai.li <blacktear23@gmail.com>
2021-05-25 16:13:43 +0800

    Fix can_introspect_foreign_keys may return True bug

--- django_tidb/tidb/features.py
+++ django_tidb/tidb/features.py
@@ -1,6 +1,11 @@
 from django.db.backends.mysql import base as mybase
+from django.utils.functional import cached_property
 
 
 class DatabaseFeatures(mybase.DatabaseFeatures):
     supports_foreign_keys = False
     uses_savepoints = False
+
+    @cached_property
+    def can_introspect_foreign_keys(self):
+        return False
